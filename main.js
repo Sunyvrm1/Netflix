adc873e25126340c66176351f9e09b2987898b28
Sunyvrm1 <verma.suny1603@gmail.com>
2023-12-16 21:51:06 +0530

    add2

--- main.js
+++ main.js
@@ -42,3 +42,74 @@ fetch("faq.json")
       });
     });
   });
+
+// shfhsdbhfbsjdv
+
+const sliders = document.querySelectorAll(".carouselbox");
+
+sliders.forEach((slider) => {
+  const switchLeft = slider.parentElement.querySelector(".switchLeft");
+  const switchRight = slider.parentElement.querySelector(".switchRight");
+
+  slider.addEventListener("scroll", function () {
+    // Check if the scroll position is at the beginning
+    if (slider.scrollLeft === 0) {
+      switchLeft.style.display = "none";
+    } else {
+      switchLeft.style.display = "block";
+    }
+
+    // Check if the scroll position is at the end
+    if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
+      switchRight.style.display = "none";
+    } else {
+      switchRight.style.display = "block";
+    }
+  });
+
+  slider.addEventListener("mouseenter", () => {
+    switchLeft.style.opacity = "1";
+    switchRight.style.opacity = "1";
+  });
+
+  slider.addEventListener("mouseleave", () => {
+    switchLeft.style.opacity = "0";
+    switchRight.style.opacity = "0";
+  });
+
+  switchLeft.addEventListener("mouseenter", () => {
+    switchLeft.style.opacity = "1";
+    switchRight.style.opacity = "1";
+  });
+
+  switchLeft.addEventListener("mouseleave", () => {
+    switchLeft.style.opacity = "0";
+    switchRight.style.opacity = "0";
+  });
+
+  switchRight.addEventListener("mouseenter", () => {
+    switchLeft.style.opacity = "1";
+    switchRight.style.opacity = "1";
+  });
+
+  switchRight.addEventListener("mouseleave", () => {
+    switchLeft.style.opacity = "0";
+    switchRight.style.opacity = "0";
+  });
+
+  switchLeft.addEventListener("click", () => {
+    const scrollAmount = window.innerWidth;
+    slider.scrollTo({
+      left: slider.scrollLeft - scrollAmount,
+      behavior: "smooth",
+    });
+  });
+
+  switchRight.addEventListener("click", () => {
+    const scrollAmount = window.innerWidth;
+    slider.scrollTo({
+      left: slider.scrollLeft + scrollAmount,
+      behavior: "smooth",
+    });
+  });
+});
