d259c55ad2aee442e2c19b7214ebbd7c4c4c6454
Sunyvrm1 <verma.suny1603@gmail.com>
2023-12-19 23:23:26 +0530

    api_issue1

--- home.js
+++ home.js
@@ -109,127 +109,197 @@ fetch(
 
     //Image clicked event
 
-    const images = document.querySelectorAll('.images img');
+    const images = document.querySelectorAll(".images img");
     images.forEach((img) => {
-      img.addEventListener('click', (event) => {
-        const clickedId = event.target.getAttribute('alt');
+      img.addEventListener("click", (event) => {
+        const clickedId = event.target.getAttribute("alt");
         storeClickedId(clickedId);
-        console.log('Clicked ID:', clickedId);
+        console.log("Clicked ID:", clickedId);
       });
     });
   });
 
 // -------------------------- Calling API 2 --------------------------
 
-fetch(
-  "https://api.themoviedb.org/3/discover/movie?api_key=d7667b78097516f5e82e6955576dcf62"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    data.results.map((arr) => {
-      const box = document.querySelector(".carouselbox1");
-      const html = `
-         <div class="images1">
-         <img src="https://image.tmdb.org/t/p/w500/${arr.backdrop_path}" alt="${arr.id}" />
-          </div>
-         `;
-      box.insertAdjacentHTML("beforeend", html);
-    });
-  });
+// fetch(
+//   "https://api.themoviedb.org/3/tv/on_the_air?api_key=d7667b78097516f5e82e6955576dcf62"
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     data.results.map((arr) => {
+//       const box = document.querySelector(".carouselbox1");
+//       const html = `
+//          <div class="images1">
+//          <img src="https://image.tmdb.org/t/p/w500/${arr.backdrop_path}" alt="${arr.id}" />
+//           </div>
+//          `;
+//       box.insertAdjacentHTML("beforeend", html);
+//     });
+//   });
 
-// -------------------------- Calling API 3 --------------------------
+// // -------------------------- Calling API 3 --------------------------
 
-fetch(
-  "https://api.themoviedb.org/3/discover/tv?api_key=d7667b78097516f5e82e6955576dcf62"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    data.results.map((arr) => {
-      const box = document.querySelector(".carouselbox2");
-      const html = `
-           <div class="images1">
-           <img src="https://image.tmdb.org/t/p/w500/${arr.backdrop_path}" alt="${arr.id}" />
-            </div>
-           `;
-      box.insertAdjacentHTML("beforeend", html);
-    });
-  });
+// fetch(
+//   "https://api.themoviedb.org/3/discover/tv?api_key=d7667b78097516f5e82e6955576dcf62"
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     data.results.map((arr) => {
+//       const box = document.querySelector(".carouselbox2");
+//       const html = `
+//            <div class="images1">
+//            <img src="https://image.tmdb.org/t/p/w500/${arr.backdrop_path}" alt="${arr.id}" />
+//             </div>
+//            `;
+//       box.insertAdjacentHTML("beforeend", html);
+//     });
+//   });
 
-// -------------------------- Calling API 4 --------------------------
+// // -------------------------- Calling API 4 --------------------------
 
-fetch(
-  "https://api.themoviedb.org/3/movie/upcoming?api_key=d7667b78097516f5e82e6955576dcf62"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    delete data.results[7];
-    data.results.map((arr) => {
-      const box = document.querySelector(".carouselbox3");
-      const html = `
-           <div class="images1">
-           <img src="https://image.tmdb.org/t/p/w500/${arr.backdrop_path}" alt="${arr.id}" />
-            </div>
-           `;
-      box.insertAdjacentHTML("beforeend", html);
-    });
-  });
+// fetch(
+//   "https://api.themoviedb.org/3/movie/upcoming?api_key=d7667b78097516f5e82e6955576dcf62"
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     delete data.results[7];
+//     data.results.map((arr) => {
+//       const box = document.querySelector(".carouselbox3");
+//       const html = `
+//            <div class="images1">
+//            <img src="https://image.tmdb.org/t/p/w500/${arr.backdrop_path}" alt="${arr.id}" />
+//             </div>
+//            `;
+//       box.insertAdjacentHTML("beforeend", html);
+//     });
+//   });
 
-// -------------------------- Calling API 5 --------------------------
+// // -------------------------- Calling API 5 --------------------------
 
-fetch(
-  "https://api.themoviedb.org/3/trending/movie/day?api_key=d7667b78097516f5e82e6955576dcf62"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    data.results.map((arr) => {
-      const box = document.querySelector(".carouselbox4");
-      const html = `
-           <div class="images1">
-           <img src="https://image.tmdb.org/t/p/w500/${arr.backdrop_path}" alt="${arr.id}" />
-            </div>
-           `;
-      box.insertAdjacentHTML("beforeend", html);
-    });
-  });
+// fetch(
+//   "https://api.themoviedb.org/3/trending/movie/day?api_key=d7667b78097516f5e82e6955576dcf62"
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     data.results.map((arr) => {
+//       const box = document.querySelector(".carouselbox4");
+//       const html = `
+//            <div class="images1">
+//            <img src="https://image.tmdb.org/t/p/w500/${arr.backdrop_path}" alt="${arr.id}" />
+//             </div>
+//            `;
+//       box.insertAdjacentHTML("beforeend", html);
+//     });
+//   });
 
-// -------------------------- Calling API 6  --------------------------
+// // -------------------------- Calling API 6  --------------------------
 
-const storeClickedId = (clickedId) => {
-  localStorage.setItem('clickedId', clickedId);
-};
+// const storeClickedId = (clickedId) => {
+//   localStorage.setItem("clickedId", clickedId);
+// };
 
-fetch(
-  "https://api.themoviedb.org/3/trending/tv/day?api_key=d7667b78097516f5e82e6955576dcf62"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
+// fetch(
+//   "https://api.themoviedb.org/3/trending/tv/day?api_key=d7667b78097516f5e82e6955576dcf62"
+// )
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
 
-    data.results.map((arr, i) => {
-      const box = document.querySelector(".carouselbox5");
-      const html = `
-           <div class="images1">
-           <img src="https://image.tmdb.org/t/p/w500/${arr.backdrop_path}" alt="${arr.id}" />
-            </div>
-           `;
-      box.insertAdjacentHTML("beforeend", html);
-    });
+//     data.results.map((arr, i) => {
+//       const box = document.querySelector(".carouselbox5");
+//       const html = `
+//            <div class="images1">
+//            <img src="https://image.tmdb.org/t/p/w500/${arr.backdrop_path}" alt="${arr.id}" />
+//             </div>
+//            `;
+//       box.insertAdjacentHTML("beforeend", html);
+//     });
 
-    //Image clicked event
+//     //Image clicked event
 
-    const images = document.querySelectorAll('.images1 img');
-    images.forEach((img) => {
-      img.addEventListener('click', (event) => {
-        const clickedId = event.target.getAttribute('alt');
-        console.log('Clicked ID:', clickedId);
-        storeClickedId(clickedId);
-        window.location.href = 'series.html'
+//     const images = document.querySelectorAll(".images1 img");
+//     images.forEach((img) => {
+//       img.addEventListener("click", (event) => {
+//         const clickedId = event.target.getAttribute("alt");
+//         console.log("Clicked ID:", clickedId);
+//         storeClickedId(clickedId);
+//         window.location.href = "series.html";
+//       });
+//     });
+//   });
+
+// Fetch and display images
+
+function fetchandDisplay(apiEndpoint, targetBox, clickHandler) {
+  fetch(apiEndpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      data.results.map((item) => {
+        const box = document.querySelector(targetBox);
+        const html = `
+        <div class="images1">
+          <img src="https://image.tmdb.org/t/p/w500/${item.backdrop_path}" alt="${item.id}" />
+        </div>`;
+        // console.log(apiEndpoint);
+        box.insertAdjacentHTML("beforeend", html);
+      });
+
+      //image clicked
+
+      const images = document.querySelectorAll(`${targetBox} img`);
+      images.forEach((img) => {
+        img.addEventListener("click", clickHandler);
       });
     });
-  });
+}
 
+// Click event handler to store clicked ID and redirect
 
+function handleImageClick(event) {
+  const clickedId = event.target.getAttribute("alt");
+  console.log("Clicked ID:", clickedId);
+  localStorage.setItem("clickedId", clickedId);
+  window.location.href = "series.html";
+}
+
+// Define API endpoints and target boxes
+
+const apiEndpoints = [
+  "https://api.themoviedb.org/3/tv/on_the_air?api_key=d7667b78097516f5e82e6955576dcf62",
+  "https://api.themoviedb.org/3/discover/tv?api_key=d7667b78097516f5e82e6955576dcf62",
+  "https://api.themoviedb.org/3/discover/movie?api_key=d7667b78097516f5e82e6955576dcf62",
+  "https://api.themoviedb.org/3/trending/movie/day?api_key=d7667b78097516f5e82e6955576dcf62",
+  "https://api.themoviedb.org/3/trending/tv/day?api_key=d7667b78097516f5e82e6955576dcf62",
+];
+
+const middleTextArray = apiEndpoints.map((url) => {
+  const startIndex = url.indexOf("/3/") + 3; // Index after "/3/"
+  const endIndex = url.indexOf("?"); // Index before the query string
+  const middleText = url.substring(startIndex, endIndex);
+  return middleText;
+
+  function handleAPIClick(event) {
+    const clickedAPI = event.target.getAttribute("alt");
+    console.log("Clicked ID:", clickedAPI);
+    localStorage.setItem("clickedAPI", clickedAPI);
+  }
+});
+
+// console.log(middleTextArray);
+
+const targetBoxes = [
+  ".carouselbox1",
+  ".carouselbox2",
+  ".carouselbox3",
+  ".carouselbox4",
+  ".carouselbox5",
+];
+
+// Loop through APIs to fetch and display images
+apiEndpoints.forEach((endpoint, index) => {
+  fetchandDisplay(endpoint, targetBoxes[index], handleImageClick);
+});
--- series.js
+++ series.js
@@ -1,30 +1,35 @@
-const storedId = localStorage.getItem('clickedId');
+const storedId = localStorage.getItem("clickedId");
 console.log(storedId);
 
+// ------------------ Api 6 calling ------------------
+
 fetch(
-    "https://api.themoviedb.org/3/trending/tv/day?api_key=d7667b78097516f5e82e6955576dcf62"
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+  "https://api.themoviedb.org/3/trending/tv/day?api_key=d7667b78097516f5e82e6955576dcf62"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
 
-      const matchingId = data.results.find((item) => item.id.toString() === storedId);
-      if(matchingId){
-        console.log("suny")
-        const name = document.getElementById("name");
-        const genre = document.getElementById("genre");
-        const overview = document.getElementById("overview");
-        const release = document.getElementById("release");
-        const rating = document.getElementById("rating");
-        const poster = document.getElementById("poster");
+    const matchingId = data.results.find(
+      (item) => item.id.toString() === storedId
+    );
+    if (matchingId) {
+      const name = document.getElementById("name");
+      const overview = document.getElementById("overview");
+      const release = document.getElementById("release");
+      const rating = document.getElementById("rating");
+      const poster = document.getElementById("poster");
+      const language = document.getElementById("language");
+      const country = document.getElementById("country");
 
-        name.innerHTML = matchingId.name;
-        genre.innerHTML = matchingId.genre_ids;
-        overview.innerHTML = matchingId.overview;
-        release.innerHTML = matchingId.first_air_date;
-        rating.innerHTML = matchingId.vote_average;
-        poster.src = 'https://image.tmdb.org/t/p/w500/' + matchingId.poster_path;
-      } else{
-        console.log('No match found');
-      }
-    });
+      name.innerHTML = matchingId.name;
+      overview.innerHTML = matchingId.overview;
+      release.innerHTML = matchingId.first_air_date;
+      rating.innerHTML = matchingId.vote_average;
+      country.innerHTML = matchingId.origin_country;
+      language.innerHTML = matchingId.original_language;
+      poster.src = "https://image.tmdb.org/t/p/w500/" + matchingId.poster_path;
+    } else {
+      console.log("No match found");
+    }
+  });
