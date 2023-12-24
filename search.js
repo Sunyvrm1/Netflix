dcd49aa14d77dbd600cc5ae8f4c7bfaa54f92343
Sunyvrm1 <verma.suny1603@gmail.com>
2023-12-24 14:42:21 +0530

    sdbfjsdj

--- search.js
+++ search.js
@@ -1,12 +1,3 @@
-//nav
-
-const navIcon = document.querySelector(".navIcon");
-const navCont = document.querySelector(".header");
-
-navIcon.addEventListener("click", () => {
-  navCont.classList.toggle("active");
-});
-
 // localStorage
 
 const storedSearch = localStorage.getItem("movieInputValue");
